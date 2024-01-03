06ba52dbe56566c18926dfd05c81c27c906699e1
zlima <endercraft68@gmail.com>
2024-01-02 22:42:07 -0300

    Refactor removeFromArray and sumAll functions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+        newArray.push(item);
+        }
+    });
+  
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    for (let i = num1; i <= num2; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
